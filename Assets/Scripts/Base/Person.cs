42bd93cb8d24c5551c0718768b6ddfef7a55ff69
FangKaiyan <402600526@qq.com>
2020-12-02 18:39:24 +0800

    _(:3」∠)_

---
+++ Assets/Scripts/Base/Person.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+    巴拉巴拉
+*/
+public class Person : Object
+{
+    
+}
---
+++ Assets/Scripts/Helper/Def.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+    公共定义类
+*/
+public struct Def
+{
+
+}
+
+public enum Gender
+{
+    Man,// 男
+    Woman,// 女
+}
+
+public enum MerchandiseType
+{
+    Commodity,// 日用品
+    Drinks,// 酒水
+    Fresh,// 生鲜
+    Food,// 食品
+    Appliance,// 家用电器
+    Furniture,// 家具
+    Stationery,// 文具、图书
+    Clothing,// 服装
+}
+
+public enum OperationType
+{
+    Add,// 添加
+    Updata,// 修改
+    Delete,// 删除
+}
+
+public enum StaffRightsType
+{
+    Admin,// 管理员
+    Ordinary,// 普通员工
+    Temporary,// 临时工
+}
+
+public enum StaffPostType
+{
+    Cashier,// 收银员
+    Guide,// 导购
+}
+
+public enum GuestType
+{
+    Vip,// 会员
+    Other,// 普通
+}
+
+public enum Receipt
+{
+
+}
