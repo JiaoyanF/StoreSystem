5ac00c4f0561d8b21a09b5427c6d3e608356e1a8
FangKaiyan <402600526@qq.com>
2020-12-08 18:06:43 +0800

    _(:3」∠)_

--- Assets/Scripts/UI/LauncherUI.cs
+++ Assets/Scripts/UI/LauncherUI.cs
@@ -9,6 +9,7 @@ using UnityEngine.UI;
 public class LauncherUI : MonoBehaviour
 {
     private GameObject title;// 标题对象
+    private GameObject center;// 中间按钮组
     private GameObject login_btn;// 登录按钮
     private GameObject close_btn;// 关闭
     private GameObject login_group;// 登录面板
@@ -19,18 +20,19 @@ public class LauncherUI : MonoBehaviour
     {
         print("Awake");
         title = Tool.FindChild(this.transform, "title").gameObject;
+        center = Tool.FindChild(this.transform, "center").gameObject;
         login_btn = Tool.FindChild(this.transform, "login_btn").gameObject;
         close_btn = Tool.FindChild(this.transform, "close_btn").gameObject;
         login_group = Tool.FindChild(this.transform, "login_group").gameObject;
         bg = Tool.FindChild(this.transform, "bg").gameObject;
-        
-        loginPanel = new LoginPanel(login_group, login_btn);
     }
     void Start()
     {
         print("Start");
         // Text title_text = title.GetComponent<Text>();
         // title_text.text = "这是一个管理系统";
+
+        loginPanel = new LoginPanel(login_group, center);
 
         title.GetComponent<Text>().text = "这是一个管理系统";
         login_btn.GetComponent<Button>().onClick.AddListener(LoginBtnClick);
@@ -44,12 +46,10 @@ public class LauncherUI : MonoBehaviour
 
     void LoginBtnClick()
     {
-        print("登录");
         if (login_group != null && login_group.activeSelf == false)
         {
             login_btn.SetActive(false);
             login_group.SetActive(true);
-            // loginPanel.Init();
         }
     }
     void CloseBtnClick()
@@ -70,8 +70,8 @@ public class LoginPanel
 {
     GameObject obj;// 自身
     GameObject mutex;// 互斥对象
-    Text un_input;
-    Text ps_input;
+    InputField un_input;
+    InputField ps_input;
 
     /// <summary>
     /// 构造
@@ -86,25 +86,31 @@ public class LoginPanel
     }
     private void Init()
     {
-        Tool.FindChild(this.obj.transform, "mask").gameObject.GetComponent<Button>().onClick.AddListener(ResetBtnClick);
-        Tool.FindChild(this.obj.transform, "login").gameObject.GetComponent<Button>().onClick.AddListener(LoginBtnClick);
-        Tool.FindChild(this.obj.transform, "reset").gameObject.GetComponent<Button>().onClick.AddListener(ResetBtnClick);
-        un_input = Tool.FindChild(this.obj.transform, "un_text").gameObject.GetComponent<Text>();
-        ps_input = Tool.FindChild(this.obj.transform, "ps_text").gameObject.GetComponent<Text>();
-        un_input.text = "000";
+        Tool.FindChild(this.obj.transform, "mask").gameObject.GetComponent<Button>().onClick.AddListener(MaskClick);
+        Tool.FindChild(this.obj.transform, "login").gameObject.GetComponent<Button>().onClick.AddListener(LoadData);
+        Tool.FindChild(this.obj.transform, "reset").gameObject.GetComponent<Button>().onClick.AddListener(ResetData);
+        un_input = Tool.FindChild(this.obj.transform, "un_input").gameObject.GetComponent<InputField>();
+        ps_input = Tool.FindChild(this.obj.transform, "ps_input").gameObject.GetComponent<InputField>();
     }
-    private void LoginBtnClick()
+    /// <summary>
+    /// 登录加载
+    /// </summary> 
+    private void LoadData()
     {
         un_input.text = "000";
         ps_input.text = "000";
     }
-    private void ResetBtnClick()
+    /// <summary>
+    /// 重置
+    /// </summary> 
+    private void ResetData()
     {
         un_input.text = "";
         ps_input.text = "";
     }
     private void MaskClick()
     {
+        ResetData();
         this.obj.SetActive(false);
         this.mutex.SetActive(true);
     }
